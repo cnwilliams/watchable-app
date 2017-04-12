683ce0b0db27aaa3425bc9993af52706938a9571
cnwilliams <hello.christenwilliams@gmail.com>
2017-04-12 15:07:21 -0700

    add list after action; BUG list not added with FB reg

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,23 @@ class User < ApplicationRecord
 
   has_many :lists, dependent: :destroy
 
+  after_create :add_list
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.name.split(" ")[0]
       user.last_name = auth.info.name.split(" ")[1]
-      # user.avatar = auth.info.image
-
+      user.avatar = auth.info.image
     end
+  end
+
+  private
+
+  def add_list
+    # self is the user that was just created
+    self.lists.create(title: "My List", genre: "Comedy")
   end
 
 end
